152baa7541d54d009ede80083b75d6f49ec6199f
Emre <emreyldrm80@gmail.com>
2024-01-04 11:04:12 +0300

    V30 reCapDemo3

---
+++ javaBasics-Kodlama.io/javademos/reCapDemo3_Classes/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        DortIslem dortIslem = new DortIslem();
+        int sonuc = dortIslem.topla(3,6);
+        System.out.println(sonuc);
+    }
+}
