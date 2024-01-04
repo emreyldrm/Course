f55628cfe460df5434e90236f756482eeb76a790
Emre <emreyldrm80@gmail.com>
2024-01-04 16:27:41 +0300

    V42-43 interfaces

---
+++ javaBasics-Kodlama.io/javademos/interfaces/src/main/java/org/example/CustomerManager.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class CustomerManager {
+
+    private ICustomerDal customerDal;
+    public CustomerManager(ICustomerDal customerDal){
+        this.customerDal = customerDal;
+    }
+    public void add(){
+        customerDal.add();
+    }
+}
