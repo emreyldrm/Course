1f137c0d8120b2c3eb96c05a7736eacd8255df20
Emre <emreyldrm80@gmail.com>
2024-01-05 02:08:00 +0300

    JS intro

---
+++ javaScriptBasics-Tobeto/E06/script.js
@@ -0,0 +1,15 @@
+var name = "Murat Başeren";
+
+function showName() {
+    console.log(name);
+}
+function sum(num1, num2) {
+    console.log(num1 + num2);
+}
+function writeText(text) {
+    console.log("writeText metodu çalıştı.")
+    document.write(text);
+}
+function bodyOnLoad() {
+    console.log("body yüklenmesi tamamlandı.");
+}
