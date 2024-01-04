99e23abe3729f294775598d42d75dc93f27db3de
Emre <emreyldrm80@gmail.com>
2024-01-04 11:48:17 +0300

    V35 methodOverloading

---
+++ javaBasics-Kodlama.io/javademos/methodOverloading/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        DortIslem dortIslem = new DortIslem();
+        System.out.println(dortIslem.topla(3,2));
+        dortIslem.topla(2,3,5);
+    }
+}
