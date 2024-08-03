7913c36915c2148648bba4530b8a67e541f60611
Emre <emreyldrm80@gmail.com>
2024-08-03 03:22:31 +0300

    flightTicket

--- javaBasics-Patika.dev/java101/flightTicket/src/main/java/org/example/Main.java
+++ javaBasics-Patika.dev/java101/flightTicket/src/main/java/org/example/Main.java
@@ -1,7 +1,48 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int distance, age, travelType;
+        double pricePerDistance = 0.10, discount;
+        boolean isError = false;
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Enter the distance: ");
+        distance = inp.nextInt();
+        System.out.print("Enter your age: ");
+        age = inp.nextInt();
+        System.out.print("Enter your travel type (1 => one way, 2 => round trip): ");
+        travelType = inp.nextInt();
+
+        double totalPrice = distance * pricePerDistance;
+
+        if (distance < 0 || age < 0 || !(travelType == 1 || travelType == 2)){
+            System.out.println("You have entered incorrect data!");
+            isError = true;
+        }
+        if (age < 12){
+            discount = totalPrice * 0.5;
+            totalPrice -= discount;
+        } else if (age < 24){
+            discount = totalPrice * 0.1;
+            totalPrice -= discount;
+        } else if (age >65) {
+            discount = totalPrice * 0.3;
+            totalPrice -= discount;
+        }
+
+        if (travelType == 2){
+            discount = totalPrice * 0.2;
+            totalPrice -= discount;
+            totalPrice *= 2;
+        }
+        if (!isError){
+            System.out.println("Total price is: " + totalPrice);
+        }
+
+
     }
 }
