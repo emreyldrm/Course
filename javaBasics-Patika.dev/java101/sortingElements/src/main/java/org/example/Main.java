30307011b6cb80bc4a6cf04ba6e607e221ece382
Emre <emreyldrm80@gmail.com>
2024-08-10 23:30:22 +0300

    entrance

---
+++ javaBasics-Patika.dev/java101/sortingElements/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+    }
+}
