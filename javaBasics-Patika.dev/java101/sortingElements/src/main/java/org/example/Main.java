131426a538b45941d92b469459fce3cb1aee4adf
Emre <emreyldrm80@gmail.com>
2024-08-11 23:28:29 +0300

    sortingElements

--- javaBasics-Patika.dev/java101/sortingElements/src/main/java/org/example/Main.java
+++ javaBasics-Patika.dev/java101/sortingElements/src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -8,5 +9,17 @@ public class Main {
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter the size of the array: ");
+        int size = input.nextInt();
+        int[] array = new int[size];
+
+        for (int i = 0; i < size ; i++) {
+            System.out.print("Enter " + (i+1) + ". number: ");
+            array[i] = input.nextInt();
+        }
+        Arrays.sort(array);
+        System.out.print("Sorted Array: ");
+        for(int i : array){ //or we can use Arrays.toString();
+            System.out.print(i + " ");
+        }
     }
 }
